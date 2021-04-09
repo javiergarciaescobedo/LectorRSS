4434dec0ae20fdd388e37c335aa153eabf2a38c8
Javier García Escobedo <javiergarbedo@gmail.com>
2021-04-09 11:30:19 +0200

    Dependencias

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,4 +1,8 @@
 module es.javiergarciaescobedo.lectorrss {
     requires javafx.controls;
+    requires java.xml.bind;
+    requires jakarta.activation;
+    requires java.activation;
+    opens es.javiergarciaescobedo.lectorrss to java.xml.bind;
     exports es.javiergarciaescobedo.lectorrss;
 }
